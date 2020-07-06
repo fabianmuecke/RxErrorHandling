f983c3c64f62a23bdfedacd53dd65e5bef1f7163
Fabian Muecke <fabian.muecke@deepl.com>
2020-07-06 18:19:43 +0200

    Added share

--- Sources/RxErrorHandling/TreatableConvertibleType.swift
+++ Sources/RxErrorHandling/TreatableConvertibleType.swift
@@ -712,3 +712,38 @@ extension TreatableConvertibleType {
         Treatable(raw: asObservable().delay(dueTime, scheduler: scheduler))
     }
 }
+
+// MARK: share
+
+extension TreatableConvertibleType {
+    /**
+     Returns an observable sequence that **shares a single subscription to the underlying sequence**, and immediately upon subscription replays  elements in buffer.
+
+     This operator is equivalent to:
+     * `.whileConnected`
+     ```
+     // Each connection will have it's own subject instance to store replay events.
+     // Connections will be isolated from each another.
+     source.multicast(makeSubject: { Replay.create(bufferSize: replay) }).refCount()
+     ```
+     * `.forever`
+     ```
+     // One subject will store replay events for all connections to source.
+     // Connections won't be isolated from each another.
+     source.multicast(Replay.create(bufferSize: replay)).refCount()
+     ```
+
+     It uses optimized versions of the operators for most common operations.
+
+     - parameter replay: Maximum element count of the replay buffer.
+     - parameter scope: Lifetime scope of sharing subject. For more information see `SubjectLifetimeScope` enum.
+
+     - seealso: [shareReplay operator on reactivex.io](http://reactivex.io/documentation/operators/replay.html)
+
+     - returns: An observable sequence that contains the elements of a sequence produced by multicasting the source sequence.
+     */
+    public func share(replay: Int = 0, scope: SubjectLifetimeScope = .whileConnected)
+        -> Treatable<Element, Failure> {
+        Treatable(raw: asObservable().share(replay: replay, scope: scope))
+    }
+}
