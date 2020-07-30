b4b6b9dbab31faad6d143896d5e93bd95ec5e59f
Fabian Muecke <fabian.muecke@deepl.com>
2020-07-30 15:46:27 +0200

    Added asTreatable for all PrimitiveSequence types

---
+++ Sources/RxErrorHandling/PrimitiveSequence+Treatable.swift
@@ -0,0 +1,46 @@
+//
+//  PrimitiveSequence+Treatable.swift
+//  
+//
+//  Created by Fabian Mücke on 30.07.20.
+//
+
+import Foundation
+import RxSwift
+
+extension PrimitiveSequence {
+    public func asTreatable() -> TreatableSequence<Trait, Element, Swift.Error> {
+        TreatableSequence(raw: asObservable())
+    }
+
+    public func asTreatable<Failure>(mapError: @escaping (Error) -> Failure) -> TreatableSequence<Trait, Element, Failure> {
+        TreatableSequence(raw: asObservable().catchError { .error(mapError($0)) })
+    }
+
+    public func asTreatable(onErrorJustReturn element: Element) -> TreatableSequence<Trait, Element, Never> {
+        TreatableSequence(raw: asObservable().catchErrorJustReturn(element))
+    }
+
+    public func asTreatable<Failure>(onErrorTreatWith: @escaping (Error) -> TreatableSequence<Trait, Element, Failure>)
+        -> Treatable<Element, Failure> {
+        TreatableSequence(raw: asObservable().catchError { error in
+            onErrorTreatWith(error).asObservable()
+        })
+    }
+
+    /// Make sure your source Observable already catches all errors and returns Result.failure instead. Otherwise using this function is unsafe.
+    public func asTreatableFromResult<Success, Failure>() -> TreatableSequence<Trait, Success, Failure>
+        where Element == Swift.Result<Success, Failure> {
+        TreatableSequence(raw: asObservable().flatMap { (element: Result<Success, Failure>) -> Observable<Success> in
+            switch element {
+            case let .success(element):
+                return .just(element)
+            case let .failure(error):
+                return .error(error)
+            }
+        }
+        .do(onError: { error in
+            rxFatalErrorInDebug("An observable which should never fail produced an error: \(error)")
+        }))
+    }
+}
--- Sources/RxErrorHandling/TreatableSingle.swift
+++ Sources/RxErrorHandling/TreatableSingle.swift
@@ -120,9 +120,3 @@ extension TreatableSequenceType where Trait == SingleTrait, Failure == Never {
         }
     }
 }
-
-extension PrimitiveSequence where Trait == SingleTrait {
-    public func asTreatable<Failure>(mapError: @escaping (Error) -> Failure) -> TreatableSingle<Element, Failure> {
-        TreatableSingle(raw: asObservable().catchError { .error(mapError($0)) })
-    }
-}
